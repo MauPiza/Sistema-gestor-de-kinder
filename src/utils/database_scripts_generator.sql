dedf71e4055d853612962fb366170938609868af
MauPiza <55927837+MauPiza@users.noreply.github.com>
2023-05-14 22:12:15 -0500

    Se actualizó el script de base de datos, se modificaron tablas

--- src/utils/database_scripts_generator.sql
+++ src/utils/database_scripts_generator.sql
@@ -1,14 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 5.0.1
+-- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 21-04-2023 a las 15:44:21
--- Versión del servidor: 10.4.11-MariaDB
--- Versión de PHP: 7.4.3
+-- Tiempo de generación: 15-05-2023 a las 05:09:05
+-- Versión del servidor: 10.4.28-MariaDB
+-- Versión de PHP: 8.0.28
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
@@ -19,60 +18,8 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Base de datos: `sistemacalificacioneskinder`
+-- Base de datos: `sistema_calificaciones_kinder`
 --
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `alumno`
---
-
-CREATE TABLE `alumno` (
-  `id_alumno` int(11) NOT NULL,
-  `id_tutor` int(11) NOT NULL,
-  `id_curso` int(11) NOT NULL,
-  `nombre_alumno` varchar(20) DEFAULT NULL,
-  `apellido_paterno` varchar(20) DEFAULT NULL,
-  `apellido_materno` varchar(20) DEFAULT NULL,
-  `fecha_nacimiento` date DEFAULT NULL,
-  `CURP` varchar(18) DEFAULT NULL,
-  `edad` char(1) DEFAULT NULL,
-  `sexo` char(1) DEFAULT NULL,
-  `grupo` char(1) DEFAULT NULL,
-  `grado` char(1) DEFAULT NULL,
-  `id_salon` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `bitacora`
---
-
-CREATE TABLE `bitacora` (
-  `id_registro` int(11) NOT NULL,
-  `id_usuario` int(11) NOT NULL,
-  `hora` time NOT NULL,
-  `fecha_salida` date DEFAULT NULL,
-  `fecha_entrada` date DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `curso`
---
-
-CREATE TABLE `curso` (
-  `id_curso` int(11) NOT NULL,
-  `id_materia` int(11) NOT NULL,
-  `id_alumno` int(11) NOT NULL,
-  `fecha_inicio` date DEFAULT NULL,
-  `fecha_final` date DEFAULT NULL,
-  `calificacion_final` int(11) DEFAULT NULL,
-  `nivel_materias` char(1) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
 
@@ -85,7 +32,7 @@ CREATE TABLE `materia` (
   `nombre_materia` varchar(30) DEFAULT NULL,
   `observaciones` varchar(100) DEFAULT NULL,
   `nivel` char(1) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Volcado de datos para la tabla `materia`
@@ -117,122 +64,9 @@ INSERT INTO `materia` (`id_materia`, `nombre_materia`, `observaciones`, `nivel`)
 (23, 'Educación socioemocional', '', '3'),
 (24, 'Informática y robótica', '', '3');
 
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `perfil_usuario`
---
-
-CREATE TABLE `perfil_usuario` (
-  `id_tipo_usuario` int(11) NOT NULL,
-  `descripcion` varchar(50) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Volcado de datos para la tabla `perfil_usuario`
---
-
-INSERT INTO `perfil_usuario` (`id_tipo_usuario`, `descripcion`) VALUES
-(0, 'Secretaria'),
-(1, 'Director-administrador');
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `salon`
---
-
-CREATE TABLE `salon` (
-  `id` int(11) NOT NULL,
-  `nombre` varchar(50) NOT NULL,
-  `cupo_maximo` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Volcado de datos para la tabla `salon`
---
-
-INSERT INTO `salon` (`id`, `nombre`, `cupo_maximo`) VALUES
-(1, '1-A', 30),
-(2, '1-B', 30),
-(3, '1-C', 30),
-(4, '1-D', 30),
-(5, '2-A', 30),
-(6, '2-B', 30),
-(7, '2-C', 30),
-(8, '2-D', 30),
-(9, '3-A', 30),
-(10, '3-B', 30),
-(11, '3-C', 30),
-(12, '3-D', 30);
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `tutor`
---
-
-CREATE TABLE `tutor` (
-  `id_tutor` int(11) NOT NULL,
-  `nombre` varchar(20) DEFAULT NULL,
-  `apellido_paterno` varchar(20) DEFAULT NULL,
-  `apellido_materno` varchar(20) DEFAULT NULL,
-  `correo_electronico` varchar(50) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `usuarios`
---
-
-CREATE TABLE `usuarios` (
-  `id_usuario` int(11) NOT NULL,
-  `perfil` int(11) DEFAULT NULL,
-  `nombre` varchar(30) DEFAULT NULL,
-  `apellido_paterno` varchar(30) DEFAULT NULL,
-  `apellido_materno` varchar(30) DEFAULT NULL,
-  `username` varchar(30) DEFAULT NULL,
-  `password` varchar(30) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Volcado de datos para la tabla `usuarios`
---
-
-INSERT INTO `usuarios` (`id_usuario`, `perfil`, `nombre`, `apellido_paterno`, `apellido_materno`, `username`, `password`) VALUES
-(1, 1, 'Angel Gabriel', 'Mena', 'Suastegui', 'amena_dir@lb.com', 'dir123'),
-(14, 0, 'Paulina Abigail', 'Vazquez', 'Navarro', 'pvazquez_sec@lb.com', '123456'),
-(15, 0, 'Vanessa Lizbeth', 'Magadan', 'Morales', 'vmagadan_sec@lb.com', 'chocolate'),
-(18, 0, 'Sofia', 'Herrera', 'Vizcaino', 'sherrera_sec@lb.com', 'prueba');
-
 --
 -- Índices para tablas volcadas
 --
-
---
--- Indices de la tabla `alumno`
---
-ALTER TABLE `alumno`
-  ADD PRIMARY KEY (`id_alumno`),
-  ADD KEY `id_tutor` (`id_tutor`),
-  ADD KEY `id_curso` (`id_curso`),
-  ADD KEY `id_salon` (`id_salon`);
-
---
--- Indices de la tabla `bitacora`
---
-ALTER TABLE `bitacora`
-  ADD PRIMARY KEY (`id_registro`),
-  ADD KEY `id_usuario` (`id_usuario`);
-
---
--- Indices de la tabla `curso`
---
-ALTER TABLE `curso`
-  ADD PRIMARY KEY (`id_curso`),
-  ADD KEY `curso_ibfk_1` (`id_materia`),
-  ADD KEY `curso_ibfk_2` (`id_alumno`);
 
 --
 -- Indices de la tabla `materia`
@@ -241,106 +75,14 @@ ALTER TABLE `materia`
   ADD PRIMARY KEY (`id_materia`);
 
 --
--- Indices de la tabla `perfil_usuario`
---
-ALTER TABLE `perfil_usuario`
-  ADD PRIMARY KEY (`id_tipo_usuario`);
-
---
--- Indices de la tabla `salon`
---
-ALTER TABLE `salon`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indices de la tabla `tutor`
---
-ALTER TABLE `tutor`
-  ADD PRIMARY KEY (`id_tutor`);
-
---
--- Indices de la tabla `usuarios`
---
-ALTER TABLE `usuarios`
-  ADD PRIMARY KEY (`id_usuario`),
-  ADD KEY `perfil` (`perfil`);
-
---
 -- AUTO_INCREMENT de las tablas volcadas
 --
-
---
--- AUTO_INCREMENT de la tabla `alumno`
---
-ALTER TABLE `alumno`
-  MODIFY `id_alumno` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT de la tabla `bitacora`
---
-ALTER TABLE `bitacora`
-  MODIFY `id_registro` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT de la tabla `curso`
---
-ALTER TABLE `curso`
-  MODIFY `id_curso` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT de la tabla `materia`
 --
 ALTER TABLE `materia`
   MODIFY `id_materia` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=25;
-
---
--- AUTO_INCREMENT de la tabla `salon`
---
-ALTER TABLE `salon`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
-
---
--- AUTO_INCREMENT de la tabla `tutor`
---
-ALTER TABLE `tutor`
-  MODIFY `id_tutor` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT de la tabla `usuarios`
---
-ALTER TABLE `usuarios`
-  MODIFY `id_usuario` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
-
---
--- Restricciones para tablas volcadas
---
-
---
--- Filtros para la tabla `alumno`
---
-ALTER TABLE `alumno`
-  ADD CONSTRAINT `alumno_ibfk_1` FOREIGN KEY (`id_tutor`) REFERENCES `tutor` (`id_tutor`),
-  ADD CONSTRAINT `alumno_ibfk_2` FOREIGN KEY (`id_curso`) REFERENCES `curso` (`id_curso`),
-  ADD CONSTRAINT `alumno_ibfk_3` FOREIGN KEY (`id_salon`) REFERENCES `salon` (`id`);
-
---
--- Filtros para la tabla `bitacora`
---
-ALTER TABLE `bitacora`
-  ADD CONSTRAINT `bitacora_ibfk_1` FOREIGN KEY (`id_usuario`) REFERENCES `usuarios` (`id_usuario`) ON DELETE CASCADE;
-
---
--- Filtros para la tabla `curso`
---
-ALTER TABLE `curso`
-  ADD CONSTRAINT `curso_ibfk_1` FOREIGN KEY (`id_materia`) REFERENCES `materia` (`id_materia`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `curso_ibfk_2` FOREIGN KEY (`id_alumno`) REFERENCES `alumno` (`id_alumno`) ON DELETE CASCADE ON UPDATE CASCADE;
-
---
--- Filtros para la tabla `usuarios`
---
-ALTER TABLE `usuarios`
-  ADD CONSTRAINT `usuarios_ibfk_1` FOREIGN KEY (`perfil`) REFERENCES `perfil_usuario` (`id_tipo_usuario`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
